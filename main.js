c58bb5967ba75b84e29c5206775ddb177cb6f9a2
ANANT JAIN <139585700+anant-jain01@users.noreply.github.com>
2024-06-28 18:34:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('.btn');
 buttons.forEach((button) => {
     button.addEventListener('click', function() {
-        let rating = this.textContent.trim(); // Added .trim() to remove whitespace
+        let rating = this.textContent.trim(); 
         let thanksDiv = document.querySelector('.thanks');
         thanksDiv.innerHTML = `You Selected ${rating} out of 5`;
         console.log('dfdf');
